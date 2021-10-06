b15a042359e7e7093fa0f1c30520c701ace0aee1
Teoman Yalçınöz <teoyalcinoz@gmail.com>
2021-10-07 00:40:03 +0300

    campergridview

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
         <v-btn depressed flat>Kadın</v-btn>
         <v-btn depressed flat>Erkek</v-btn>
         <v-btn depressed flat>Çocuk</v-btn>
+
         <v-spacer></v-spacer>
         <v-text-field
           append-icon="mdi-magnify"
@@ -25,6 +26,37 @@
         </v-btn>
       </v-toolbar>
     </v-card>
+
+    <v-row>
+      <v-col class="d-flex child-flex" sm="12" cols="12">
+        <v-img
+          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/22fa517d-a949-4119-a210-cbb08370536a/T93_Peu-Stedium_2000x1125_desktop.avif={}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        ></v-img>
+      </v-col>
+      <v-col class="d-flex child-flex" cols="6">
+        <v-img
+          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/2429b0c7-0187-4d37-b5e4-ef9d3293b1ae/T93-Gender-Walden-Women-desktop.avif={}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        ></v-img>
+      </v-col>
+      <v-col class="d-flex child-flex" cols="6">
+        <v-img
+          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/7cf1e10a-0c2f-4505-9835-c52f97182916/T93-Gender-Walden-Men-desktop.avif={}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        ></v-img>
+      </v-col>
+      <v-col class="d-flex child-flex" cols="12">
+        <v-img
+          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/c565ea73-9386-4b44-bd51-8389176cde27/T93_Store-locator-Dist-desktop.avif={}`"
+          aspect-ratio="1"
+          class="grey lighten-2"
+        ></v-img>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -35,12 +67,7 @@
 import Navbar from "./components/Navbar";
 export default {
   data: () => ({
-    items: [
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me" },
-      { title: "Click Me 2" },
-    ],
+    items: ["All", "Family", "Friends", "Coworkers"],
   }),
   name: "app",
   components: {
