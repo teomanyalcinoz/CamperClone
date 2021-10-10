add5d0b908d7cbd54d3b23d40c333d7ff7aae4ee
Teoman Yalçınöz <teoyalcinoz@gmail.com>
2021-10-11 01:25:07 +0300

    ortalamaca

--- src/App.vue
+++ src/App.vue
@@ -28,76 +28,48 @@
     </v-card>
 
     <v-row>
-
-      <v-col class="d-flex child-flex" cols="12" sm="12"> 
+      <v-col class="d-flex child-flex" cols="12" sm="12">
         <v-card>
-        <v-img
-          src="./assets/camper_gif.gif"
-          aspect-ratio="2"
-          class="images">
-         
-          
-            <span class="lilspan">
-              11. Sayı: Kıbrıs<br>
-            </span>
+          <v-img src="./assets/camper_gif.gif" aspect-ratio="2" class="images">
+            <v-text class="lilspan"> 11. Sayı: Kıbrıs </v-text>
+            <v-spacer></v-spacer>
+            <v-text class="gifspan"> The Walking Society<br /> </v-text>
 
-            <span class="gifspan">
-              The Walking Society<br>
-            </span>
-
-            <v-btn
-              depressed
-              elevation="2"
-              floating
-              class="butonlar"
-              id="btn1"
-            >Kampanyaya Göz Atın</v-btn>
-          
-        </v-img>
+            <v-btn depressed elevation="2" floating class="butonlar" id="btn1"
+              >Kampanyaya Göz Atın</v-btn
+            >
+          </v-img>
         </v-card>
       </v-col>
-      
+
       <v-col class="d-flex child-flex" sm="12" cols="12">
         <v-card>
-        <v-img
-          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/22fa517d-a949-4119-a210-cbb08370536a/T93_Peu-Stedium_2000x1125_desktop.avif={}`"
-          aspect-ratio="2"
-          class="images">
-
-          <div class="itemsdiv">
-            <span class="gifspan">
-              Peu Stadium<br>
-            </span>
-            <span class="lilspan">
-              Koleksiyonu Keşfet<br>
-            </span>
-            <v-btn
-              depressed
-              elevation="2"
-              floating
-              class="butonlar"
-              id="btn2"
-            >Daha Fazlası</v-btn>
-          </div>
-        </v-img>
+          <v-img
+            :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/22fa517d-a949-4119-a210-cbb08370536a/T93_Peu-Stedium_2000x1125_desktop.avif={}`"
+            aspect-ratio="2"
+            class="images"
+          >
+            <v-text class="peuspan"> PeuStadium </v-text>
+            <v-spacer></v-spacer>
+            <v-text class="colspan"> Koleksiyonu Keşfet<br /> </v-text>
+            <v-btn depressed elevation="2" floating class="butonlar" id="btn2"
+              >Daha Fazlası</v-btn
+            >
+          </v-img>
         </v-card>
       </v-col>
-      
+
       <v-col class="d-flex child-flex" cols="6">
         <v-card>
-        <v-img
-          :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/2429b0c7-0187-4d37-b5e4-ef9d3293b1ae/T93-Gender-Walden-Women-desktop.avif={}`"
-          aspect-ratio="1"
-          class="images"
-        >
-        <v-btn
-              depressed
-              elevation="2"
-              floating
-              class="butonlar"
-              id="btn3"
-            >Kadın</v-btn>
-        </v-img>
+          <v-img
+            :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/2429b0c7-0187-4d37-b5e4-ef9d3293b1ae/T93-Gender-Walden-Women-desktop.avif={}`"
+            aspect-ratio="1"
+            class="images"
+          >
+            <v-btn depressed elevation="2" floating class="butonlar" id="btn3"
+              >Kadın</v-btn
+            >
+          </v-img>
         </v-card>
       </v-col>
 
@@ -105,14 +77,11 @@
         <v-img
           :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/7cf1e10a-0c2f-4505-9835-c52f97182916/T93-Gender-Walden-Men-desktop.avif={}`"
           aspect-ratio="1"
-          class="images">
-          <v-btn
-              depressed
-              elevation="2"
-              floating
-              class="butonlar"
-              id="btn4"
-            >Erkek</v-btn>
+          class="images"
+        >
+          <v-btn depressed elevation="2" floating class="butonlar" id="btn4"
+            >Erkek</v-btn
+          >
         </v-img>
       </v-col>
 
@@ -120,17 +89,13 @@
         <v-img
           :src="`https://www.camper.com/cms/api/assets/tiers-banners-homes/c565ea73-9386-4b44-bd51-8389176cde27/T93_Store-locator-Dist-desktop.avif={}`"
           aspect-ratio="2"
-         class="images">
-         <v-btn
-              depressed
-              elevation="2"
-              floating
-              class="butonlar"
-              id="btn5"
-            >En Yakın Mağazayı Bul</v-btn>
+          class="images"
+        >
+          <v-btn depressed elevation="2" floating class="butonlar" id="btn5"
+            >En Yakın Mağazayı Bul</v-btn
+          >
         </v-img>
       </v-col>
-
     </v-row>
     <v-footer padless>
       <v-card flat tile class="text-center">
@@ -173,9 +138,6 @@
 </template>
 
 <style scoped src="@/assets/styles/style.css">
-
-
-
 </style>
 
 <script>
