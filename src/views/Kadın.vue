05169d506a6c352dc38b5001033cc48dad38c11a
bulutyelken <190101052@ogrenci.yalova.edu.tr>
2022-01-16 01:27:12 +0300

    Revert "Merge branch 'master' of https://github.com/teomanylcnz/camper-project-vue into HEAD"
    This reverts commit 35554758233209ceed92960b7c7a6e99e49a164d, reversing
    changes made to eff0d4f6c8cf28455d17816471f0c32019597f21.

--- src/views/Kadın.vue
+++ src/views/Kadın.vue
@@ -4,17 +4,13 @@
         <v-btn to="urundty">Urun detay</v-btn>
     
     <Filt />
-    <v-row id="row" style="padding-top:50px;">
-      
+    <v-row id="row">
       <v-col
         v-for="(item, index) in this.$store.state.itemskadin"
         :key="index"
         cols="3"
-        style="padding:0px;"
       >
-      <div id="divayakkabi">
-      <a href="javascript:void(0)" class="fill-div">
-        <v-img :src="item.image" aspect-ratio="1">
+        <v-img :src="item.image" aspect-ratio="1" class="grey lighten-2">
           <template v-slot:placeholder>
             <v-row class="fill-height ma-0" align="center" justify="center">
               <v-progress-circular
@@ -24,37 +20,13 @@
             </v-row>
           </template>
         </v-img>
-        <div style="display:flex; flex-direction:column; margin-left:30px; margin-top:16px; margin-bottom:20px; color:black;">
-          <span style="z-index:1; margin-bottom:6px;">Twins</span>
-          <span style="z-index:1">₺1.599</span>
-        </div>
-        </a>
-      </div>
       </v-col>
-      
     </v-row>
   </div>
 </template>
 
 
 <style scoped>
-#app{
-  position: unset;
-}
-
-#ayakkabidiv{
-  height: auto;
-  width: auto;
-  
-}
-
-.fill-div{
-  display: block;
-  height: 100%;
-  width: 100%;
-  text-decoration: none;
-}
-
 #baslık {
   margin-left: 680px;
   margin-top: 90px;
@@ -69,7 +41,6 @@
 
 <script>
 import Filt from "../components/Filter.vue";
-
 
 export default {
   beforeCreate() {
